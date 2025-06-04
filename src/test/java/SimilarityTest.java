82d6996d5ef0b4a3b3607a668b4e6e060665429d
kfc <kfc@example.com>
2025-06-04 16:12:47 +0900

    add test for not null similarity object

--- src/test/java/SimilarityTest.java
+++ src/test/java/SimilarityTest.java
@@ -1,5 +1,19 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SimilarityTest {
 
+    private Similarity sm;
+
+    @BeforeEach
+    void setUp() {
+        sm = new Similarity();
+    }
+
+    @Test
+    void notNullSimilarityObject() {
+        assertNotNull(sm);
+    }
 }
